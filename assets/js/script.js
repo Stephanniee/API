7b591ff64bb1a61906f8aabdb583cd29ed75e465
Stephannie M <rumbi.m@hotmail.co.uk>
2024-01-29 18:30:06 +0000

    API GET request for key

---
+++ assets/js/script.js
@@ -0,0 +1,26 @@
+const API_KEY = "vLmuvuEgunMa-C25YrIAzo2Vdho";
+const API_URL = "https://ci-jshint.herokuapp.com/api";
+const resultsModal = new bootstrap.Modal(document.getElementById("resultsModal"));
+
+document.getElementById("status").addEventListener("click", e => getStatus(e));
+
+async function getStatus(e){
+    const queryString = `${API_URL}?api_key=${API_KEY}`;
+    const reponse = await fetch(queryString);
+    const data = await reponse.json();
+    if (reponse.ok){
+        //.expiry shows the expiray date only
+        displayStatus(data.expiry);
+    }else {
+        throw new Error(data.error);
+    }
+}
+function displayStatus(data){
+    let heading ="API Key Status"
+    let results =`<div> Your key is valid until</div>`;
+    results += `<div class="key-status".${data.expiry}</div>`;
+
+    document.getElementById("resultsModalTitle").innerText = heading;
+    document.getElementById("results-content").innerHTML = results;
+    resultsModal.show();
+}
